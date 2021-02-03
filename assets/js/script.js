0f5c1d1e6d3120e2b370461d6bfcc871fd705ee3
Jonathan Goldstein <jonathangoldsteine@gmail.com>
2021-02-03 17:36:41 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
-hello
+//  Creating the function for the list of cities and creating an append in jquery to add more cities to the list
+function createCityList(citySearchList) {
+    $("#city-list").empty();
+  
+    var keys = Object.keys(citySearchList);
+    //for loop
+    for (var i = 0; i < keys.length; i++) {
+      var cityListEntry = $("<button>");
+      cityListEntry.addClass("list-group-item list-group-item-action");
+  
+      var splitStr = keys[i].toLowerCase().split(" ");
+      for (var j = 0; j < splitStr.length; j++) {
+        splitStr[j] =
+          splitStr[j].charAt(0).toUpperCase() + splitStr[j].substring(1);
+      }
+      var titleCasedCity = splitStr.join(" ");
+      cityListEntry.text(titleCasedCity);
+  
+      $("#city-list").append(cityListEntry);
+    }
+  }
